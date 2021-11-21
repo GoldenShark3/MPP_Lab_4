5d2084e6e93e4cf4bce23e0e761d11b2d7c0f3f4
GoldenShark3 <vslecha2013@mail.ru>
2021-11-21 12:58:43 +0300

    Complete lab

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Core.TestGenerator.Implementations;
+
+namespace ConsoleApp
+{
+    static class TestProgram
+    {
+        static void Main()
+        {
+            var generator = NUnitTestGenerator.CreateGenerator(builder =>
+            {
+                builder.MaxGenerateFiles = 10;
+                builder.MaxReadFiles = 10;
+                builder.MaxWriteFiles = 10;
+            });
+            generator.GenerateTestsAsync(@"C:\Users\Aleksey\Desktop\TestsGenerator-MPP-4\ConsoleApp\input",
+                @"C:\Users\Aleksey\Desktop\TestsGenerator-MPP-4\ConsoleApp\output").Wait();
+        }
+    }
+}
